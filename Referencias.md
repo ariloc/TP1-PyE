6281d4533e4172f7dfb2654b41e5c828cac45b8c
Lorl <grassiantonella747@gmail.com>
2025-04-21 02:11:25 -0300

    Modificación archivo alquiler + imagen del gráfico del mismo.

--- Referencias.md
+++
@@ -1,5 +0,0 @@
-# Referencias
-[1] La Poderosa (2022). Observatorio Villero, [El Gato y la Caja](https://lapoderosa.elgatoylacaja.com/) </br>
-[2] Infoleg (2017), Texto completo de la norma, [Decreto 358/2017](https://servicios.infoleg.gob.ar/infolegInternet/anexos/275000-279999/275037/norma.htm) </br>
-[3] Ministerio de Desarrollo Social (2021). [Consultoría técnica para el diseño de estrategias de regularización dominial de hogares inquilinos en los barrios populares - Informe final](https://www.argentina.gob.ar/sites/default/files/2021/05/informe_final_hogares_inquilinos_renabap_14_marzo_2022.pdf) </br>
-[4] Infobae (2022, 9 de octubre). [En Argentina, más de cinco millones de personas viven en barrios populares, villas o asentamientos.](https://www.infobae.com/politica/2022/10/09/en-argentina-mas-de-cinco-millones-de-personas-viven-en-barrios-populares-villas-o-asentamientos/#:~:text=En%20los%20últimos%20seis%20años,grados%20de%20precariedad%20y%20hacinamiento)
--- reporte/alquiler.R
+++ reporte/alquiler.R
@@ -1,32 +1,10 @@
 library(here)
 source(here("datos","leer-datos-lp.R"))
 
-library(tidyverse)
-
 #Costo promedio del alquiler en los barrios populares:
 datos_alquiler <- datos %>%
   filter(!is.na(costo_alquiler)) %>%
   count(costo_alquiler)
-
-#Gráfico
-ggplot(datos_alquiler, aes(x = as.factor(costo_alquiler), y = n)) +
-  geom_bar(stat = "identity", fill = "indianred2", color = "black") +
-  geom_text(aes(label = n), vjust = 2, color = "#8B0000", size = 3.5) + #etiquetas dentro de rectángulos.
-  labs(
-    title = "Frecuencia del costo del alquiler en barrios populares",
-    x = "Costo del alquiler en pesos",
-    y = "Cantidad de viviendas",
-  ) +theme(
-    plot.tag.position = "bottom",
-    plot.title = element_text(size = 12, face = "bold", hjust = 0.5,vjust=-2)) + 
-  ylim(0, max(datos_alquiler$n) + 1) +  # Ajuste dinámico del eje Y
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-
-#Calculamos la media aritmética del coste del alquiler:
-media_alquiler <- mean(datos$costo_alquiler, na.rm = TRUE)
-print(media_alquiler)
-#mediana_alquiler<-median(datos$costo_alquiler, na.rm = TRUE)
 
 #Calculemos el mínimo y máximo:
 min_alquiler <- min(datos$costo_alquiler, na.rm = TRUE)
@@ -34,3 +12,30 @@ print(min_alquiler)
 max_alquiler <- max(datos$costo_alquiler, na.rm = TRUE)
 print(max_alquiler)
 
+
+#Calculamos el nro de intervalos (√n)
+n <- nrow(datos_alquiler)
+rango <-max_alquiler - min_alquiler
+binwidth <- rango / sqrt(n)
+
+#Gráfico
+ggplot(datos_alquiler, aes(x = costo_alquiler)) +
+  geom_histogram(binwidth = binwidth, fill = "indianred2", color = "black") +
+  labs(
+    title = "Frecuencia del costo del alquiler en barrios populares",
+    x = "Costo del alquiler en pesos",
+    y = "Cantidad de viviendas",
+  ) +theme(
+    plot.tag.position = "bottom",
+    plot.title = element_text(size = 12, face = "bold", hjust = 0.5,vjust=-2), 
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  ) +
+  theme_minimal()
+
+#Calculamos la media aritmética del coste del alquiler:
+media_alquiler <- mean(datos$costo_alquiler, na.rm = TRUE)
+print(media_alquiler)
+#mediana_alquiler<-median(datos$costo_alquiler, na.rm = TRUE)
+
+#Calculamos el desvío estándar:
+desvio_estandar_alquiler<-sd(datos$costo_alquiler,na.rm=TRUE)
