2d4105d1972656232506146043d64fae78838958
Lorl <grassiantonella747@gmail.com>
2025-05-22 19:41:03 -0300

    Completar análisis faltante , re-nombramiento de variables para mejorar comprensión.

--- reporte/distribucion-tenencia.R
+++ reporte/distribucion-tenencia.R
@@ -6,6 +6,7 @@ library(ggrepel)
 
 # Para introducir los datos, queremos visualizar la proporción de personas que residen en una vivienda propia y 
 # la proporción que residen en una no propia
+
 # NOTA: Agruparemos todo lo que sea distinto de "propio" en "no propio" (Es decir,Prestado-Alquilado-Ocupado-Otro)
 # Además, agruparemos en "propio" la vivienda "Propio con título" y "Propio sin título".
 
@@ -36,7 +37,6 @@ ggplot(datos_grafico_tenencia, aes(x = 3 , y = valor, fill = fct_inorder(grupo))
   guides(fill = guide_legend(title = "Tipo de tenencia de la vivienda")) + #Leyenda
   labs(  #Título y nota
     title = "Distribución de los hogares según tipo de tenencia de la vivienda\nen los barrios relevados",
-    # caption="NOTA: Los datos de vivienda propia con comprobante de propiedad y sin título se han agrupado en vivienda propia \n Los datos de vivienda alquilada, prestada, ocupada u otro se han agrupado en vivienda no propia",
     caption = "Fuente: Relevamiento de Condiciones Habitacionles 2022, La Poderosa"
   ) +
   #Estilo del gráfico
@@ -51,6 +51,8 @@ ggplot(datos_grafico_tenencia, aes(x = 3 , y = valor, fill = fct_inorder(grupo))
     legend.margin = margin(10, 10, 10, 10)  # Espacio entre el borde de la leyenda y los elementos
   )
 
+#------------------------------
+
 # Realizaremos la moda de cada grupo, para averiguar cuál es el que lo lidera
 moda_tenencia_no_propia<-datos_refactorizados %>%
   filter(propiedad_reclasificada == "No propia") %>%
@@ -61,4 +63,66 @@ moda_tenencia_propia<-datos_refactorizados %>%
   filter(propiedad_reclasificada == "Propia") %>%
   count(propiedad, sort = TRUE)%>%
   slice(1)
+
+#--------------------------------
+
+# Filtramos por tipo de tenencia y contamos por subgrupo dentro de la misma.
+datos_tenencia_no_propia <- datos_refactorizados %>%
+  filter(propiedad_reclasificada == "No propia") %>%
+  count(propiedad)
+
+datos_tenencia_propia <- datos_refactorizados %>%
+  filter(propiedad_reclasificada == "Propia") %>%
+  count(propiedad)
+
+# Calculamos el total de cada grupo
+total_tenencia<-datos_refactorizados %>%
+  count(propiedad_reclasificada)
+
+
+# Porcentajes dentro de cada tipo de tenencia (Ejemplo: El x% de la tenencia no propia está constituida por...)
+total_tenencia_no_propia_tabla_porcentajes<-datos_tenencia_no_propia %>%
+  mutate(porcentaje = round(n / sum(n) * 100, 1))
+
+total_tenencia_propia_tabla_porcentajes<-datos_tenencia_propia %>%
+  mutate(porcentaje = round(n / sum(n) * 100, 1))
+
+
+##---------------------
+
+# Gráfico de barras: tenencia no propia
+ggplot(datos_tenencia_no_propia, aes(x = fct_reorder(propiedad, n), y = n)) +
+  geom_col(fill = "#FF9999", color = "black") +
+  geom_text(aes(label = n), hjust = 0.5, size = 4,vjust=4) +  #Etiquetas de cantidad (n) 
+  labs(
+    title = "Distribución de tipos de tenencia entre viviendas no propias",
+    x = "Tipo de tenencia",
+    y = "Cantidad de viviendas",
+    caption = "Fuente: Relevamiento de Condiciones Habitacionales 2022, La Poderosa"
+  ) +
+  theme_classic() +
+  theme(
+    plot.title = element_text(face = "bold", size = 12, hjust = 0.5),
+    axis.text = element_text(size = 10),
+    plot.caption = element_text(size = 8, hjust = 0)
+  ) +
+  expand_limits(y = max(datos_tenencia_no_propia$n) * 1.1)  # Espacio para que el número no se corte
+
+# Gráfico de barras: tenencia propia
+ggplot(datos_tenencia_propia, aes(x = fct_reorder(propiedad, n), y = n)) +
+  geom_col(fill = "#ad5e9d", color = "black") +
+  geom_text(aes(label = n), hjust = 0.5, size = 4,vjust=5) +  
+  labs(
+    title = "Distribución de tipos de tenencia entre viviendas propias",
+    x = "Tipo de tenencia",
+    y = "Cantidad de viviendas",
+    caption = "Fuente: Relevamiento de Condiciones Habitacionales 2022, La Poderosa"
+  ) +
+  theme_classic() +
+  theme(
+    plot.title = element_text(face = "bold", size = 12, hjust = 0.5),
+    axis.text = element_text(size = 10),
+    plot.caption = element_text(size = 8, hjust = 0)
+  ) +
+  expand_limits(y = max(datos_tenencia_propia$n) * 1.1) 
 
