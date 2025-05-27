31b579dc4177d77c0c32c67a018bee3472368a45
Ariel Leonardo Fideleff <sefide77@gmail.com>
2025-05-27 20:26:48 -0300

    Cambiar gráfico de humedad a barras

--- reporte/humedad-ambientes.R
+++ reporte/humedad-ambientes.R
@@ -3,14 +3,14 @@ source(here("datos","leer-datos-lp.R"))
 
 library(tidyverse)
 
-#Analizaremos los problemas de humedad en los diferentes ambientes de la vivienda de los barrios populares
+# Analizaremos los problemas de humedad en los diferentes ambientes de la vivienda de los barrios populares
 
 datos_humedad <- datos %>%
   select(
     humedad_dormitorios, humedad_cocina,humedad_baño,humedad_living,humedad_otro
   )
 
-#Como las respuetas no cuentan con "sí"/"no" , las refactorizamos:
+# Como las respuestas no cuentan con "sí"/"no" , las refactorizamos:
 datos_humedad_expandido <- datos_humedad %>%
   mutate(
     dormitorio = !is.na(humedad_dormitorios) & humedad_dormitorios != "",
@@ -20,7 +20,7 @@ datos_humedad_expandido <- datos_humedad %>%
     otro = !is.na(humedad_otro) & humedad_otro != "",
   )
 
-#TRUE-FALSE
+# TRUE-FALSE
 datos_humedad_largo <- datos_humedad_expandido %>%
   pivot_longer(
     cols = c(dormitorio, cocina, baño, living, otro),
@@ -28,38 +28,50 @@ datos_humedad_largo <- datos_humedad_expandido %>%
     values_to = "hay_humedad"
   )
 
-#Los ordeno para no mezclar valores:
-datos_humedad_largo<-datos_humedad_largo %>%
+# Los ordeno para no mezclar valores:
+datos_humedad_largo <- datos_humedad_largo %>%
   mutate(hay_humedad=factor(hay_humedad,levels=c(TRUE,FALSE)))
 
-#Calculamos porcentajes  - agrupamos datos.
+# Calculamos porcentajes  - agrupamos datos.
 datos_humedad_resumen <- datos_humedad_largo %>%
   group_by(ambiente, hay_humedad) %>%
   summarize(cant = n(), .groups = "drop_last") %>%
   mutate(
-    porcentaje = round(cant / sum(cant) * 100, 1)
+    porcentaje = round(cant / sum(cant), 3) # Redondeado a un dígito al multiplicar por 100
   )
+
+# Nos quedamos con sólo con los TRUE
+datos_humedad_resumen <- datos_humedad_resumen %>%
+  filter(hay_humedad == TRUE)
+
+# Cambiamos finalmente los nombres de los ambientes para la gráfica
+datos_humedad_resumen <- datos_humedad_resumen %>%
+  mutate(ambiente = 
+           factor(ambiente, 
+                  levels = c("cocina","dormitorio","baño","living","otro"),
+                  labels = c("Cocina","Dormitorio","Baño","Living","Otro")
+           )
+  )
+  
 
 # Gráfico
 ggplot(datos_humedad_resumen) +
-  aes(x = ambiente, y = porcentaje, fill = hay_humedad) +
+  aes(x = reorder(ambiente, porcentaje), y = porcentaje, fill = ambiente) + # Ordenar por frecuencia de mayor a menor con reorder()
   geom_bar(position = "dodge", stat = "identity", colour = "black") +
-  scale_fill_manual(
-    values = c("TRUE" = "#63B8FF", "FALSE" = "#CD96CD"),
-    labels = c("Sí", "No")
-  ) +
+  scale_y_continuous(labels = scales::percent) +  # Usar etiquetas de porcentaje en la escala
   labs(
     x = "Ambiente de la vivienda",
-    y = "Porcentaje de humedad",
-    fill = "Presencia de humedad",
-    title = "Distribución de problemas de humedad por ambiente\nsegún declaración en los barrios relevados",
+    y = "Porcentaje de viviendas con humedad",
+    fill = "Ambientes con humedad",
+    title = "Distribución de problemas de humedad por ambiente en las viviendas\nde los barrios relevados",
     caption = "Fuente: Relevamiento de Condiciones Habitacionales 2022, La Poderosa"
   ) +
   geom_text(
-    aes(y = porcentaje / 2, label = paste0(porcentaje, "%")),
+    aes(y = porcentaje / 2, label = paste0(porcentaje * 100, "%")),
     position = position_dodge(width = 0.9),
     size = 4.5
   ) +
+  coord_flip() +
   theme_classic() +
   theme(
     plot.tag.position = "bottom",
@@ -71,33 +83,11 @@ ggplot(datos_humedad_resumen) +
     legend.margin = margin(10, 10, 10, 10)
   )
 
-# Veamos la cantidad de cada uno (+ porcentaje):
-print(datos_humedad_resumen)
-
-
-# Calculamos proporción de humedad promedio por ambiente
-porcentaje_humedad_promedio <- datos_humedad_largo %>%
-  mutate(hay_humedad = as.logical(hay_humedad)) %>%  # <- Conversión TRUE/FALSE
-  group_by(ambiente) %>%
-  summarize(
-    promedio_humedad = mean(hay_humedad, na.rm = TRUE) * 100,
-    .groups = "drop"
-  ) %>%
-  arrange(desc(promedio_humedad))
-
-print(porcentaje_humedad_promedio)
-
-# Calculamos la mediana  de humedad por ambiente
-porcentaje_humedad_mediana <- datos_humedad_largo %>%
-  mutate(hay_humedad = as.logical(hay_humedad)) %>%  # <- Conversión TRUE/FALSE
-  group_by(ambiente) %>%
-  summarize(
-    mediana_humedad = median(hay_humedad, na.rm = TRUE) * 100,
-    .groups = "drop"
-  ) %>%
-  arrange(desc(mediana_humedad))
-
-print(porcentaje_humedad_mediana)
-#Si es 1: >50% humedad.
-#Si es 0: <50% humedad.
+# Veamos la cantidad de cada uno en un tabla (+ porcentaje):
+print(
+  datos_humedad_resumen %>% 
+  select(-hay_humedad) %>%
+  arrange(desc(porcentaje)) %>%
+  mutate(porcentaje = porcentaje*100)
+)
 
