1683df43f383d586eaab66f0a83caa982f69d270
emiliamolo <emiliamoloeznik@gmail.com>
2025-05-28 08:22:46 -0300

    Arreglo grafico edad jefe/tiempo residencia

--- reporte/edad_vs_tiempo_residencia_disp.R
+++ reporte/edad_vs_tiempo_residencia_disp.R
@@ -5,9 +5,12 @@ library(ggplot2)
 
 ggplot(datos) +
   aes(x = edad_jefe_hogar, y = tiempo_residencia) +
-  geom_point() +
+  geom_jitter(width = 0.3, height = 0.3, alpha = 0.75, color = "#252850", size = 2) +
+  #geom_smooth(method = "loess", se = TRUE, color = "darkred", fill = "red", alpha = 0.1) +
   labs(x = "Edad del jefe del hogar (años)", 
        y = "Tiempo de residencia (años)",
        caption = "Fuente: Relevamiento de Condiciones Habitacionles 2022, La Poderosa",
-       title = "Relación entre la edad del jefe del hogar\n y la cantidad máxima de personas por habitación")+
+       title = "Relación entre la edad del jefe del hogar y el tiempo de residencia")+
+  coord_fixed(ratio = 1, xlim = c(0, 80), ylim = c(0, 80)) +
   theme_classic()
+
