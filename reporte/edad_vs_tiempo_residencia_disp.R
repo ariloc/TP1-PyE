80f404e1eedb6e917fb9784a61e280722815ba5d
Ariel Leonardo Fideleff <sefide77@gmail.com>
2025-06-02 14:03:50 -0300

    Estandarizar estilo en dispersión edad jefe vs tiempo residencia. Arreglar typo.

--- reporte/edad_vs_tiempo_residencia_disp.R
+++ reporte/edad_vs_tiempo_residencia_disp.R
@@ -7,10 +7,20 @@ ggplot(datos) +
   aes(x = edad_jefe_hogar, y = tiempo_residencia) +
   geom_jitter(width = 0.3, height = 0.3, alpha = 0.75, color = "#252850", size = 2) +
   #geom_smooth(method = "loess", se = TRUE, color = "darkred", fill = "red", alpha = 0.1) +
-  labs(x = "Edad del jefe del hogar (años)", 
+  labs(x = "Edad del jefe/a del hogar (años)", 
        y = "Tiempo de residencia (años)",
-       caption = "Fuente: Relevamiento de Condiciones Habitacionles 2022, La Poderosa",
-       title = "Relación entre la edad del jefe del hogar y el tiempo de residencia")+
+       caption = "Fuente: Relevamiento de Condiciones Habitacionales 2022, La Poderosa",
+       title = "Relación entre la edad del jefe/a del hogar y el tiempo de residencia\n en los barrios relevados")+
   coord_fixed(ratio = 1, xlim = c(0, 80), ylim = c(0, 80)) +
-  theme_classic()
+  theme_classic() +
+  theme(
+    plot.tag.position = "bottom",
+    plot.title = element_text(size = 12, face = "bold", hjust = 0.5, vjust=2), 
+    plot.caption = element_text(size=8, hjust=0),
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    
+    legend.background = element_rect(color = "black", linewidth  = 0.5), 
+    legend.spacing = unit(0.5, "cm"),
+    legend.margin = margin(10, 10, 10, 10)
+  )
 
