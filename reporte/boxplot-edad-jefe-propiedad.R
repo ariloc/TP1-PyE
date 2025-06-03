472d917b470561a9ddbc88fa314760d952db3d43
Lorl <grassiantonella747@gmail.com>
2025-06-03 00:42:20 -0300

    Modificación del gráfico para seguir estándar de los mismos.

--- reporte/boxplot-edad-jefe-propiedad.R
+++ reporte/boxplot-edad-jefe-propiedad.R
@@ -22,8 +22,8 @@ ggplot(datos_jefe_propiedad) +
   geom_boxplot(fill = "indianred2") +
   labs(
     x = "Condición de propiedad", 
-    y = "Edad del jefe del hogar",
-    title = "Distribución de la edad del jefe del hogar según condición de propiedad en los barrios relevados",
+    y = "Edad del jefe/a del hogar",
+    title = "Distribución de la edad del jefe/a del hogar según condición de propiedad en los barrios relevados",
     caption = "Fuente: Relevamiento de Condiciones Habitacionales 2022, La Poderosa"
   ) +
   scale_y_continuous(breaks = seq(0, 100, 5)) +
@@ -34,4 +34,21 @@ ggplot(datos_jefe_propiedad) +
     plot.caption = element_text(size=8, hjust=0),
     axis.text.x = element_text(angle = 45, hjust = 1)
   )
+
+
+# Resumen estadístico de la relación entre la edad del jefe/a del hogar y la condición de propiedad.
+datos_jefe_propiedad %>%
+  group_by(propio) %>%
+  summarise(
+    cantidad = n(),
+    media = mean(edad_jefe_hogar, na.rm = TRUE),
+    min = min(edad_jefe_hogar, na.rm = TRUE),
+    Q1 = quantile(edad_jefe_hogar, 0.25, na.rm = TRUE),
+    mediana = median(edad_jefe_hogar, na.rm = TRUE),
+    Q3 = quantile(edad_jefe_hogar, 0.75, na.rm = TRUE),
+    max = max(edad_jefe_hogar, na.rm = TRUE),
+    RI = Q3-Q1,
+    `Desvío estándar` = sd(edad_jefe_hogar, na.rm = TRUE)
+  )
+
 
