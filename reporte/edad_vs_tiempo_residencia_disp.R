4520c2f019821f9ee112aadabed9b57eaccef4f6
Lorl <grassiantonella747@gmail.com>
2025-06-03 00:36:37 -0300

    Añadir comentario, con alternativa en el tipo de tendencia lineal.

--- reporte/edad_vs_tiempo_residencia_disp.R
+++ reporte/edad_vs_tiempo_residencia_disp.R
@@ -6,7 +6,7 @@ library(ggplot2)
 ggplot(datos) +
   aes(x = edad_jefe_hogar, y = tiempo_residencia) +
   geom_jitter(width = 0.3, height = 0.3, alpha = 0.75, color = "#252850", size = 2) +
-  #geom_smooth(method = "loess", se = TRUE, color = "darkred", fill = "red", alpha = 0.1) +
+  #geom_smooth(method = "lm", se = TRUE, color = "darkred", fill = "red", alpha = 0.1) +
   labs(x = "Edad del jefe/a del hogar (años)", 
        y = "Tiempo de residencia (años)",
        caption = "Fuente: Relevamiento de Condiciones Habitacionales 2022, La Poderosa",
@@ -23,4 +23,3 @@ ggplot(datos) +
     legend.spacing = unit(0.5, "cm"),
     legend.margin = margin(10, 10, 10, 10)
   )
-
