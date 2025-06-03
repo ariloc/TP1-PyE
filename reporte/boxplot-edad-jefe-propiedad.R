61534ae8e9c6673b620f2b43b19f10845a35019e
Ariel Leonardo Fideleff <sefide77@gmail.com>
2025-06-03 12:46:20 -0300

    Agregar marcado de regiones en boxplots edad jefe vs tenencia

--- reporte/boxplot-edad-jefe-propiedad.R
+++ reporte/boxplot-edad-jefe-propiedad.R
@@ -17,7 +17,7 @@ datos_jefe_propiedad <- datos_limpios %>%
   )
 
 # Análisis bivariado mediante boxplots comparativos
-ggplot(datos_jefe_propiedad) +
+grafico <- ggplot(datos_jefe_propiedad) +
   aes(x = propio, y = edad_jefe_hogar) +
   geom_boxplot(fill = "indianred2") +
   labs(
@@ -34,10 +34,10 @@ ggplot(datos_jefe_propiedad) +
     plot.caption = element_text(size=8, hjust=0),
     axis.text.x = element_text(angle = 45, hjust = 1)
   )
-
+grafico
 
 # Resumen estadístico de la relación entre la edad del jefe/a del hogar y la condición de propiedad.
-datos_jefe_propiedad %>%
+datos_jefe_resumen <- datos_jefe_propiedad %>%
   group_by(propio) %>%
   summarise(
     cantidad = n(),
@@ -51,4 +51,22 @@ datos_jefe_propiedad %>%
     `Desvío estándar` = sd(edad_jefe_hogar, na.rm = TRUE)
   )
 
+# Contar menores en no propias
+datos_jefe_propiedad %>%
+ filter(propio == "No propia" & edad_jefe_hogar < 18)
+
+## Marcar ciertas regiones en la gráfica
+# Mediana
+dat <- ggplot_build(grafico)$data[[1]]
+grafico_med <- grafico +
+  geom_segment(data = dat, aes(x = xmin, xend = xmax, y = middle, yend = middle), colour = c("red","blue"), size = 1)
+grafico_med
+
+# Mediana + RI
+grafico_med_ri <- grafico_med +
+  annotate("rect", xmin = 0.55, xmax = 1.45, ymin = as.numeric(datos_jefe_resumen[1,"Q1"]) - 2, ymax = as.numeric(datos_jefe_resumen[1,"Q3"]) + 2,
+           alpha = 0.2, fill = "#FFD700") +
+  annotate("rect", xmin = 1.55, xmax = 2.45, ymin = as.numeric(datos_jefe_resumen[2,"Q1"]) - 2, ymax = as.numeric(datos_jefe_resumen[2,"Q3"]) + 2,
+           alpha = 0.2, fill = "#FFD700")
+grafico_med_ri
 
